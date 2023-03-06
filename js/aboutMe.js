a5ce7b83f0310bc607296d0c549bb091c96f39d3
Jihana030 <wldnjs7618@naver.com>
2023-03-06 16:12:16 +0900

    abom ani add

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -2,19 +2,19 @@
     'use strict';
 
     // text animation
-    var string = "안녕하세요!";
-    var string2 = "제가 궁금해지셨군요.";
-    var string3 = "환영합니다.";
-    var str = string.split("");
-    var str2 = string2.split("");
-    var str3 = string3.split("");
-    var el = document.querySelector('.wel_ment');
-    (function animate() {
-        str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-        var running = setTimeout(animate, 180);
-        str2.length > 0 ? el.innerHTML += str2.shift() : clearTimeout(running); 
-        var running = setTimeout(animate, 360);
-        str3.length > 0 ? el.innerHTML += str3.shift() : clearTimeout(running); 
-        var running = setTimeout(animate, 500);        
-    })();
+    // var string = "안녕하세요!";
+    // var string2 = "제가 궁금해지셨군요.";
+    // var string3 = "환영합니다.";
+    // var str = string.split("");
+    // var str2 = string2.split("");
+    // var str3 = string3.split("");
+    // var el = document.querySelector('.wel_ment');
+    // (function animate() {
+    //     str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+    //     var running = setTimeout(animate, 180);
+    //     str2.length > 0 ? el.innerHTML += str2.shift() : clearTimeout(running); 
+    //     var running = setTimeout(animate, 360);
+    //     str3.length > 0 ? el.innerHTML += str3.shift() : clearTimeout(running); 
+    //     var running = setTimeout(animate, 500);        
+    // })();
 })()
