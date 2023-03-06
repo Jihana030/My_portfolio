bf737c2c71653de8f02b3ba632ae5d85511364a6
Jihana030 <wldnjs7618@naver.com>
2023-03-06 16:25:16 +0900

    abom click event

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,20 +1,19 @@
 (function(){
     'use strict';
 
-    // text animation
-    // var string = "안녕하세요!";
-    // var string2 = "제가 궁금해지셨군요.";
-    // var string3 = "환영합니다.";
-    // var str = string.split("");
-    // var str2 = string2.split("");
-    // var str3 = string3.split("");
-    // var el = document.querySelector('.wel_ment');
-    // (function animate() {
-    //     str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-    //     var running = setTimeout(animate, 180);
-    //     str2.length > 0 ? el.innerHTML += str2.shift() : clearTimeout(running); 
-    //     var running = setTimeout(animate, 360);
-    //     str3.length > 0 ? el.innerHTML += str3.shift() : clearTimeout(running); 
-    //     var running = setTimeout(animate, 500);        
-    // })();
+    // 이력서 다운 멘트
+    const coYes = document.querySelector('.co-yes');
+    const coNo = document.querySelector('.co-no');
+    const coThank = document.querySelector('.co-thank');
+    const coSad = document.querySelector('.co-sad');
+    coYes.addEventListener('click', e=>{
+        coYes.classList.toggle('co-dnone');
+        coNo.classList.toggle('co-dnone');
+        coThank.classList.toggle('co-dnone');
+    })
+    coNo.addEventListener('click', e=>{
+        coYes.classList.toggle('co-dnone');
+        coNo.classList.toggle('co-dnone');
+        coSad.classList.toggle('co-dnone');
+    })
 })()
