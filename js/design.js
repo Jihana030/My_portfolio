3a48b2909f18da0428add0c40e2bcca7a2fb280c
Jihana030 <wldnjs7618@naver.com>
2023-03-07 14:08:40 +0900

    slide idx event

--- js/design.js
+++ js/design.js
@@ -1,14 +1,42 @@
 (function(){
     'use strict'
 
-    // swiper
-    var swiper = new Swiper(".mySwiper", {
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        loop: true, 
-        slidesPerView: 1,
-    });
+    //explain 제목 설명
+    const exTitle = document.querySelector('.ex-title');
+    const exDetail = document.querySelector('.ex-detail');
+    // 슬라이드 엑티브
+    const sidActive = document.querySelector('.swiper-slide-active');
+    
     // json fetch
+    function loadItems(){
+        return fetch("../js/data.json")
+        .then((response)=>response.json())
+        .then((json)=>json.design);
+    }
+    loadItems().then((design)=>{
+        design.forEach((ele, idx) => {
+            
+        // swiper
+        var swiper = new Swiper(".mySwiper", {
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            // loop: true, //루프 버그 극혐
+            slidesPerView: 1,
+            on: {
+                activeIndexChange : function() {
+                    //이벤트 또는 조건문으로 이용하면 된다.
+                    console.log(`${swiper.realIndex}, ${idx}`);
+                    if(swiper.realIndex === (idx)){
+                        exTitle.innerHTML = `${ele.title}`;
+                        exDetail.innerHTML = `${ele.detail}`;
+                    }
+                },
+            }
+        });
+            
+        })
+    })
+    
 })()
